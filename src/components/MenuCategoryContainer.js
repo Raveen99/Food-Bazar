ab4f67f4019dfc24a80c92e74fe38b4de4cb3dcf
Raveen99 <raveenrn6@gmail.com>
2024-02-24 01:55:12 +0530

    Improving dropdown functionality in menu in restaurants

--- src/components/MenuCategoryContainer.js
+++ src/components/MenuCategoryContainer.js
@@ -5,27 +5,45 @@ import { useState } from "react";
 const MenuCategoryContainer = ({ resMenu }) => {
   const { title } = resMenu?.card?.card;
   const { itemCards, categories } = resMenu?.card?.card;
-  console.log("Data in Menu: ", itemCards ? itemCards : categories);
+  //console.log("Data in Menu: ", itemCards ? itemCards : categories);
 
-  const iconStyle = { fontSize: "1.6rem", transform: "rotate(180deg)" };
+  const [openCategory, setOpenCategory] = useState(
+    itemCards ? title : categories.map((category) => category.title)
+  );
 
-  const [isOpen, setIsOpen] = useState(true);
-  const expandMenu = () => {
-    setIsOpen(!isOpen);
+  console.log("Open Category: ", openCategory);
+
+  const toggleDropDown = (categoryTitle) => {
+    setOpenCategory((prevOpenCategory) => {
+      if (itemCards)
+        return prevOpenCategory === categoryTitle ? "" : categoryTitle;
+      else {
+        return prevOpenCategory.includes(categoryTitle)
+          ? prevOpenCategory.filter((title) => title !== categoryTitle)
+          : [...prevOpenCategory, categoryTitle];
+      }
+    });
   };
+
   return itemCards ? (
     <div>
       <button
         id="menuDropDown"
-        onClick={expandMenu}
-        className="menu-dropdown menu-dropdown-open"
+        onClick={() => toggleDropDown(title)}
+        className="menu-dropdown"
       >
         <div className="menu-item-heading">{`${title} (${itemCards.length})`}</div>
         <span className="menu-expand">
-          <MdKeyboardArrowUp style={iconStyle} />
+          <MdKeyboardArrowUp
+            style={{
+              fontSize: "1.6rem",
+              transform:
+                openCategory === title ? "rotate(360deg)" : "rotate(180deg)",
+            }}
+          />
         </span>
       </button>
-      {isOpen && (
+      {openCategory === title && (
         <div id="menuContainer">
           {itemCards.map((items) => (
             <div>
@@ -39,17 +57,27 @@ const MenuCategoryContainer = ({ resMenu }) => {
   ) : (
     <div>
       <div className="menu-category-heading">{title}</div>
-      {categories.map((items) => (
-        <div>
-          <button onClick={expandMenu} className="menu-dropdown">
-            <div className="menu-category-title">{`${items?.title} (${items?.itemCards.length})`}</div>
+      {categories.map((category) => (
+        <div id={category.title} className="category-menu">
+          <button
+            onClick={() => toggleDropDown(category.title)}
+            className="menu-dropdown"
+          >
+            <div className="menu-category-title">{`${category?.title} (${category?.itemCards.length})`}</div>
             <span className="menu-expand">
-              <MdKeyboardArrowUp style={iconStyle} />
+              <MdKeyboardArrowUp
+                style={{
+                  fontSize: "1.6rem",
+                  transform: openCategory.includes(category.title)
+                    ? "rotate(360deg)"
+                    : "rotate(180deg)",
+                }}
+              />
             </span>
           </button>
-          {isOpen && (
+          {openCategory.includes(category.title) && (
             <div>
-              {items.itemCards.map((items) => (
+              {category.itemCards.map((items) => (
                 <div>
                   <MenuItem key={items?.card?.info?.id} data={items} />
                   <div className="menu-items-divider"></div>
