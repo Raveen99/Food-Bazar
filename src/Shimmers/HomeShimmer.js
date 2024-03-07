e5f961cbbf5042aa7b6df3257321ecd7b5f07a9c
Raveen99 <raveenrn6@gmail.com>
2024-03-08 01:41:25 +0530

    Making Shimmer responsive and changing css to Tailwind

--- src/Shimmers/HomeShimmer.js
+++ src/Shimmers/HomeShimmer.js
@@ -5,7 +5,7 @@ const HomeShimmer = () => {
     <div>
       <BannerShimmer />
       <div className="flex flex-col w-full h-full pt-4">
-        <div className="mx-auto grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+        <div className="mx-auto grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
           <RestaurantCardShimmer />
           <RestaurantCardShimmer />
           <RestaurantCardShimmer />
