140f101656dd16c53188a63efaed6871e23a2f18
Raveen99 <raveenrn6@gmail.com>
2024-03-13 00:14:11 +0530

    Using custom hook to fetch restaurant data.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,60 +1,27 @@
-import { useState, useEffect } from "react";
-import { resURL } from "../utils/constants";
 import Banner from "./Banner";
 import TopRest from "./TopRest";
 import OnlineRest from "./OnlineRest";
 import HomeShimmer from "../Shimmers/HomeShimmer";
-
+import useResData from "../hooks/useResData";
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurant] = useState([]);
-  const [resTitle, setResTitle] = useState("");
-  const [topRestaurant, setTopRestaurant] = useState([]);
-  const [topResTitle, setTopRestTitle] = useState("");
-  const [bannerData, setBannerData] = useState([]);
+  const {
+    bannerData,
+    topResTitle,
+    topRestaurant,
+    resTitle,
+    listOfRestaurants,
+  } = useResData();
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const data = await fetch(resURL);
-
-    const restaurantData = await data.json();
-    console.log("Restaurant Data: ", restaurantData);
-    //Banner Data
-    setBannerData(
-      restaurantData?.data?.cards[0]?.card?.card?.imageGridCards?.info.map(
-        (bannerInfo) => ({
-          id: bannerInfo.id,
-          imageId: bannerInfo.imageId,
-          action: bannerInfo.action,
-        })
-      )
-    );
-    // Top Restautants
-    setTopRestaurant(
-      restaurantData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
-    setTopRestTitle(restaurantData?.data?.cards[1]?.card?.card?.header?.title);
-
-    //Online Restaurants
-    setListOfRestaurant(
-      restaurantData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
-    setResTitle(restaurantData?.data?.cards[2]?.card?.card?.title);
-  };
-  //console.log("List Of Restaurant: ", listOfRestaurants);
+  console.log("List Of Restaurants: ", listOfRestaurants);
   return listOfRestaurants.length === 0 ? (
     <HomeShimmer />
   ) : (
     <div className="w-full h-full pt-4">
       <Banner data={bannerData} />
-      <hr className="border-solid border-1 border-[rgb(240, 240, 245)] m-8 calc(10% + 52px)"></hr>
+      <hr className="border-solid border-1 border-[rgb(240, 240, 245)] m-6 calc(10% + 52px)"></hr>
 
       <TopRest resData={topRestaurant} resTitle={topResTitle} />
-      <hr className="border-solid border-1 border-[rgb(240, 240, 245)] m-8 calc(10% + 52px)"></hr>
+      <hr className="border-solid border-1 border-[rgb(240, 240, 245)] m-6 calc(10% + 52px)"></hr>
 
       <OnlineRest resTitle={resTitle} resData={listOfRestaurants} />
     </div>
---
+++ src/hooks/useResData.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { resURL } from "../utils/constants";
+
+const useResData = () => {
+  const [bannerData, setBannerData] = useState([]);
+  const [topResTitle, setTopRestTitle] = useState("");
+  const [topRestaurant, setTopRestaurant] = useState([]);
+  const [resTitle, setResTitle] = useState("");
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(resURL);
+    const restaurantData = await data.json();
+    console.log("Restaurant Data: ", restaurantData);
+    //Banner Data
+    setBannerData(
+      restaurantData?.data?.cards[0]?.card?.card?.imageGridCards?.info.map(
+        (bannerInfo) => ({
+          id: bannerInfo.id,
+          imageId: bannerInfo.imageId,
+          action: bannerInfo.action,
+        })
+      )
+    );
+    // Top Restautants
+    setTopRestaurant(
+      restaurantData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setTopRestTitle(restaurantData?.data?.cards[1]?.card?.card?.header?.title);
+
+    //Online Restaurants
+    setListOfRestaurant(
+      restaurantData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setResTitle(restaurantData?.data?.cards[1]?.card?.card?.header?.title);
+  };
+
+  return {
+    bannerData,
+    topResTitle,
+    topRestaurant,
+    resTitle,
+    listOfRestaurants,
+  };
+};
+
+export default useResData;
